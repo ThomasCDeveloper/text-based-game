9929c06a02193069f6a2eb2aa56cef6f633579bd
ThomasCDeveloper <tchaumont@proton.me>
2024-01-20 08:45:36 +0100

    added object, no use for objects

--- main.go
+++ main.go
@@ -12,14 +12,17 @@ var rooms = map[string]Room{}
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	rooms["room"] = Room{"You find yourself in a dark room... You see a \033[4mdoor\033[0m.", map[string]string{"door": "end"}}
-	rooms["end"] = Room{"Nothing to see here... You can go back where you woke up in the \033[4mroom\033[0m.", map[string]string{"room": "room"}}
+	// rooms["room"] = Room{"You find yourself in a dark room... You see a \033[4mdoor\033[0m.", map[string]string{"door": "end"}}
+	rooms["room"] = Room{"You find yourself in a dark room... You see a #Bdoor#B.", map[string]string{"door": "end"}, []string{"key"}, map[string]string{"key": "There is a #Bkey#B on the ground."}}
+	rooms["end"] = Room{"Nothing to see here... You can go back where you woke up in the \033[4mroom\033[0m.", map[string]string{"room": "room"}, []string{}, map[string]string{}}
 
 	position := "room"
+	bag := []string{"key"}
 
 	for {
-		rooms[position].show()
+		rooms[position].show(bag)
 
+		fmt.Printf("> ")
 		input, _ := reader.ReadString('\n')
 		input = input[:len(input)-1]
 		words := strings.Split(input, " ")
@@ -32,10 +35,9 @@ func main() {
 			if len(words) == 1 {
 				fmt.Println("  help: prints this message\n  exit: exits the game\n  goto <place>: attemps to go to <place>")
 			} else {
-				fmt.Println("Help for " + words[1])
 				switch words[1] {
 				case "goto":
-					fmt.Println("Valid destinations are often underlined.")
+					fmt.Println("  Valid destinations are often underlined.")
 				default:
 					fmt.Println(words[1] + " is not a valid command. Type \"help\" to see the valid commands.")
 				}
@@ -47,5 +49,6 @@ func main() {
 				position = exit
 			}
 		}
+		fmt.Println()
 	}
 }
--- room.go
+++ room.go
@@ -1,12 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
 
 type Room struct {
 	description string
 	exits       map[string]string
+
+	objects        []string
+	objDescription map[string]string
 }
 
-func (room Room) show() {
-	fmt.Println(room.description)
+func replaceInString(in string, del string, rep1 string, rep2 string) string {
+	parts := strings.Split(in, del)
+	for i := 0; i < len(parts)-1; i += 2 {
+		parts[i] = parts[i] + rep1
+	}
+	for i := 2; i < len(parts); i += 2 {
+		parts[i] = rep2 + parts[i]
+	}
+	return strings.Join(parts, "")
+}
+
+func (room Room) show(bag []string) {
+	description := room.description
+
+	for _, objName := range room.objects {
+		if !slices.Contains(bag, objName) {
+			description += " " + room.objDescription[objName]
+		}
+	}
+
+	description = replaceInString(description, "#B", "\033[4m", "\033[0m")
+	fmt.Println(description)
 }
